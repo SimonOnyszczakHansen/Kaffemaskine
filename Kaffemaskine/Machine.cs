d43bca06ee289eccbce398a9da2343dbd964c9c9
Simon Onyszczak Hansen <simo401t@zbc.dk>
2023-05-10 13:20:56 +0200

    Add project files.

---
+++ Kaffemaskine/Machine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffemaskine
+{
+    internal class Machine
+    {
+        bool isTurnedOn;
+
+        public Machine()
+        {
+            isTurnedOn = false;
+        }
+    }
+}
---
+++ Kaffemaskine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kaffemaskine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
